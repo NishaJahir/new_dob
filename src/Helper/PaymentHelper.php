7562b130460a9528b730d9eecbdf3d6793b5f4fd
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-11-28 12:38:54 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -222,13 +222,13 @@ class PaymentHelper
         }
         
         $invoicePrepaymentDetails =  [
-              'invoice_bankname'  => 'Raiffeisenlandesbank O Zndl Sddeutschland-----test1-------test2----environment',
+              'invoice_bankname'  => $requestData['invoice_bankname'],
               'invoice_bankplace' => $requestData['invoice_bankplace'],
               'invoice_iban'      => $requestData['invoice_iban'],
-              'invoice_bic'       => $requestData['invoice_bic'],
-              'due_date'          => $requestData['due_date'],
-              'invoice_type'      => $requestData['invoice_type'],
-              'invoice_account_holder' => $requestData['invoice_account_holder']
+              'invoice_bic'       => $requestData['invoice_bic']
+            //  'due_date'          => $requestData['due_date'],
+            //  'invoice_type'      => $requestData['invoice_type'],
+           //   'invoice_account_holder' => $requestData['invoice_account_holder']
                ];
            
         $invoiceDetails = json_encode($invoicePrepaymentDetails);
